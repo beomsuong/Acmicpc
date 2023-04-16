ae41ef80876463385d0e7ec91baeba2dcd06119a
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-04-16 23:10:52 +0900

    Create 구구단_2739.cpp

---
+++ 구구단_2739.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b=0;
+	cin >> a;
+	for (int i = 1;i <= 9;i++) {
+		cout << a << " * " << i << " = " << a * i << endl;
+	}
+}
