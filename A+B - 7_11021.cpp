3169ca7b190747bc85034c9b77407169dc5ee376
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-04-16 23:31:56 +0900

    Create A+B - 7_11021.cpp

---
+++ A+B - 7_11021.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b, c;
+	cin >> a;
+	for (int i = 1;i <=a;i++) {
+		cin >> b >> c;
+		cout << "Case #" << i << ": " << b + c << endl;
+	}
+}
