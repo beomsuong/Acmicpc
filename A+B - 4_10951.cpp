3dad9bdcfe2855b26de3626873145efe277435a7
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-05-04 04:14:12 +0900

    Create A+B - 4_10951.cpp

---
+++ A+B - 4_10951.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int a,b;
+	while (cin >> a >> b) {
+		cout << a + b << endl;
+	}
+}
