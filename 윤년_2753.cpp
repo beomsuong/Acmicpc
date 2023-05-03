2e2cb978fc7bba81ead06f252ff9b96dfdac2c9e
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-05-04 04:00:29 +0900

    Create 윤년_2753.cpp

---
+++ 윤년_2753.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	if (a % 4 == 0 && a % 100 != 0 ) {
+		cout << "1";
+	}
+	else if (a % 4 == 0 && a % 400 == 0) {
+		cout << "1";
+	}
+	else
+		cout << "0";
+	return 0;
+}
