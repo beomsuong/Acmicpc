16f07e16b9b49aef6bcef610c1274fed932cec5d
beomsuong <129273417+beomsuong@users.noreply.github.com>
2023-11-01 22:19:08 +0900

    Create 크냐?_4101.cpp

---
+++ 크냐?_4101.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	while (true) {
+		cin >> a >> b;
+		if (a == b && a == 0)
+			break;
+		cout <<( a > b ? "Yes" : "No") << endl;
+	}
+
+}
